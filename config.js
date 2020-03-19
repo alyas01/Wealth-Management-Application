a6238ab6c1e6f69f18912329e9ab5edbe6880b80
alyas01 <misha.alyas01@gmail.com>
2020-03-18 21:31:55 -0400

    Uploading First Working Version of Wealth Management Application using Cosmos DB

---
+++ config.js
@@ -0,0 +1,16 @@
+const config = {};
+
+config.host = process.env.HOST || "https://wealth-mgmt-fa-page.documents.azure.com:443/";
+config.authKey =
+  process.env.AUTH_KEY || "zxZFHVpFAAaz2c6gVwTTmPRQZx2T1op8LkVf37cuEVovv5ZsUHYVIO3VqSmMzi9CfQav1LpIllokukQWLMmc5Q==";
+config.databaseId = "Client";
+config.containerId = "Items";
+
+if (config.host.includes("https://localhost:")) {
+  console.log("Local environment detected");
+  console.log("WARNING: Disabled checking of self-signed certs. Do not have this code in production.");
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  console.log(`Go to http://localhost:${process.env.PORT || '3000'} to try the sample.`);
+}
+
+module.exports = config;
